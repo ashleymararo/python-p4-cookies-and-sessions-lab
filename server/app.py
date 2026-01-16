f393531e4e21b9419d175c58f419e39f18dc5b4d
ashleymararo <ashleymararo222@gmail.com>
2026-01-16 23:44:34 +0300

    finished lab

--- server/app.py
+++ server/app.py
@@ -27,8 +27,28 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Initialize if not present 
+    session['page_views'] = session['page_views'] if 'page_views' in session else 0
+    #it increase
+    session['page_views'] += 1
+    # limit
+    if session['page_views'] <= 3:
+        article = {
+            "id": id,
+            "title":f"Article{id}",
+            "content": "This is a premium blog article.",
+            "author": "Anonymous",
+            "preview": "This is a preview of the article",
+            "minutes_to_read": 5,
+            "date" : "2024-01-01"
+        }
+        return jsonify(article), 200
+    else:
+        return jsonify({
+            "message": "Maximum pageview limit reached"
+        }), 401
 
-    pass
+
 
 if __name__ == '__main__':
     app.run(port=5555)
